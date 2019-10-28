94eeb8f393a1f75dc882c2663eaf0b4779e255c9
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-28 17:54:09 -0400

    met all specs

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
